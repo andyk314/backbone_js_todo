4aa90508f81df6e86eb09335b96fddd4b7579ba2
Andy Kwong <andyk314@gmail.com>
2015-06-27 11:35:06 -0700

    Add collection of models to store data and tie it to the view.

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 (function($){ // self-executing wrapper
 
+  // create a model, javascript object, key-value pairs
+  var Item = Backbone.Model.extend({
+    defaults: {
+      part1: 'hello',
+      part2: 'world'
+    }
+  });
+
+  // create a collection of items, an array of model objects
+  var List = Backbone.Collection.extend({
+    model: Item
+  });
+
   var ListView = Backbone.View.extend({
     el: $('body'), // attaches 'this.el' to an existing element
 
@@ -10,7 +23,11 @@
 
     // automatically called upon instantiation. where you make all types of bindings, excluding UI events, such as clicks
     initialize: function(){
-      _.bindAll(this, 'render', 'addItem'); // fixes loss of context for 'this' within methods
+      _.bindAll(this, 'render', 'addItem', 'appendItem'); // fixes loss of context for 'this' within methods
+
+      // instantiate a Collection, and binds add event to its own method. backbone doesnt offer separate controller for bindings
+      this.collection = new List();
+      this.collection.bind('add', this.appendItem); // collection event binder
 
       this.counter = 0; // total number of items added thus far
       this.render(); // not all views are self-rendering. This one is. 
@@ -18,14 +35,30 @@
 
     // function in charge of render entire view in this.el. needs to be manually called by user
     render: function(){
+
+      var self = this;
       $(this.el).append("<button id='add'>Add list item</button>"); // introduces a button to add a new list item
       $(this.el).append("<ul></ul>");
+      // reference this so it can be accessed within scope of the callback
+      _(this.collection.models).each(function(item){
+        self.appendItem(item);
+      }, this);
     },
 
     // custon function called via click event above
+    // now deals solely with model/collections. View updates are move to the event listern appendItem.
     addItem: function(){
       this.counter++;
-      $('ul', this.el).append("<li>hello world"+this.counter+"</li>");
+      var item = new Item();
+      item.set({
+        part2: item.get('part2') + this.counter
+      });
+      this.collection.add(item); // add item to collection
+    },
+
+    // appendItem is triggered by collection event add and handles the visual update
+    appendItem: function(item){
+      $('ul', this.el).append("<li>"+item.get('part1')+ " "+item.get('part2')+"</li>");
     }
   });
 
